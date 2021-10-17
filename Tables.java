c8a94c55c889492794b9d97054b22b8bf7e7b089
Pratik Jadhav <thepratikjadhav@gmail.com>
2021-10-17 12:38:47 +0530

    Added Tables.java

---
+++ Tables.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Tables {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter a number");
+        int a = sc.nextInt();
+
+        System.out.println(a);
+        System.out.println(a*2);
+        System.out.println(a*3);
+        System.out.println(a*4);
+        System.out.println(a*5);
+        System.out.println(a*6);
+        System.out.println(a*7);
+        System.out.println(a*8);
+        System.out.println(a*9);
+        System.out.println(a*10);
+
+    }
+}
